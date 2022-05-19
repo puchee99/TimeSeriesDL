fa245cbbdd87f44875cba9090202ba18663167d2
puche99 <arnau.puche@e-campus.uab.cat>
2022-05-19 02:56:22 +0200

    data

--- pytrends_data.py
+++ pytrends_data.py
@@ -5,6 +5,9 @@ from pytrends.request import TrendReq
 from yahoofinancials import YahooFinancials
 import numpy as np
 
+
+
+#----------------------pytrends--------------------------
 def historical_pytrends(start = (datetime.now() - timedelta(days=265)).strftime("%Y-%m-%d"), 
                         end = datetime.now().strftime("%Y-%m-%d"), keys = ["stock","call","put"], cat = "0", geo="", gprop=""):
     df = pd.DataFrame()
@@ -43,12 +46,16 @@ def all_pytrends(start = (datetime.now() - timedelta(days=265)).strftime("%Y-%m-
 
     return df 
 
+#----------------------yahoo--------------------------
 
+
+#----------------------utils--------------------------
 def df_add_improve_cols(df):
     for col in df:
           dfx = df[col]
           df[col+"%"] = np.asarray([((dfx[x] / dfx[x-1]+1e-3) -1) * 100 if x>0 else 0 for x in range(len(dfx))])
     return df.iloc[1:] #we cannot calculate improvement on 1st row
+
 
 if __name__ == "__main__":
     df = all_pytrends()
---
+++ yahoo_financials_data.py
@@ -0,0 +1,2 @@
+from yahoofinancials import YahooFinancials
+
